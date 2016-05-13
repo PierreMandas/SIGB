65af6e8c141f4a158a200889694a98431356318e
philiprileyitu <panr@itu.dk>
2016-05-14 01:23:36 +0200

    more changes to PCA. added concenience method to allow printing multiple or single images. was just confusing before.

--- All assignments/Assignments/_03/pca/eigen_faces_pca.py
+++ All assignments/Assignments/_03/pca/eigen_faces_pca.py
@@ -14,12 +14,12 @@ class PCA():
         # <001> read images
         path = "./Assignments/_03/pca/data"
         [self.__X, self.__y] = read_images(path)
+        self.__num_components = 0
         
     def run(self):
 
         # <002> perform a full pca
-        num_components = 100
-        [eigenvalues, eigenvectors, mu] = pca(asRowMatrix(self.__X), self.__y, num_components)
+        [eigenvalues, eigenvectors, mu] = pca(asRowMatrix(self.__X), self.__y, self.__num_components)
 
         # turn the first (at most ) 16 eigenvectors into grayscale
         # images ( note : eigenvectors are stored by column!)
@@ -35,29 +35,46 @@ class PCA():
         # <006> use different number of eigen faces to construct a face from dataset
         # How many components do you think are enough for realistic reconstruction of a face?
         # (select a face in the data set)
-        
-        E = []
-        # FOR PRINTING IMAGES IN STEPS
-        #steps = [20]
-        #for i in range(16):
-            #last = steps[-1]
-            #proj = project(eigenvectors[:, 0:last], self.__X[0].reshape(1,-1), mu)
-            #recon = reconstruct(eigenvectors[:, 0:last], proj, mu)
-            
-            #recon = recon.reshape(self.__X[0].shape)
-            #E.append(normalize(recon, 0, 255))     
-            #steps.append(last + 20)  # Add a new step
             
         #print steps
         
         # FOR PRINTING SINGLE IMAGE - change rows, cols from 4 to 1!!!
-        steps = [num_components]
-        proj = project(eigenvectors[:, 0:num_components], self.__X[0].reshape(1,-1), mu)
-        recon = reconstruct(eigenvectors[:, 0:num_components], proj, mu)
-                    
-        recon = recon.reshape(self.__X[0].shape)
-        E.append(normalize(recon, 0, 255))          
+
         
         # plot reconstructed face
-        subplot(title = "Reconstruction", images = E, rows = 1, cols = 1, sptitle = "Eigenvectors", 
-                sptitles = steps , colormap =cm.gray , filename = None)    
+        #subplot(title = "Reconstruction", images = E, rows = 1, cols = 1, sptitle = "Eigenvectors", 
+                #sptitles = steps , colormap =cm.gray , filename = None)
+                
+        E = self.__reconstruct(eigenvectors, mu, 1, 1, None)
+                
+    def __reconstruct(self, eigenvectors, mu, numrows=4, numcols=4, numsteps=20):
+        '''
+        Reconstruct the faces from eigenvectors. 
+        Change numrows, numcols to 1 and numsteps to None to show single image
+        '''
+        E = []
+        if numsteps is not None:
+            # Multiple images
+            steps = [numsteps]
+            for i in range(16):
+                last = steps[-1]
+                proj = project(eigenvectors[:, 0:last], self.__X[0].reshape(1,-1), mu)
+                recon = reconstruct(eigenvectors[:, 0:last], proj, mu)
+        
+                recon = recon.reshape(self.__X[0].shape)
+                E.append(normalize(recon, 0, 255))     
+                steps.append(last + 20)  # Add a new step              
+        else:
+            # Single image
+            if self.__num_components == 0:
+                self.__num_components = 400
+            steps = [self.__num_components]
+            proj = project(eigenvectors[:, 0:self.__num_components], self.__X[0].reshape(1,-1), mu)
+            recon = reconstruct(eigenvectors[:, 0:self.__num_components], proj, mu)
+        
+            recon = recon.reshape(self.__X[0].shape)
+            E.append(normalize(recon, 0, 255))     
+            
+        subplot(title = "Reconstruction", images = E, rows = numrows, cols = numcols, sptitle = "Eigenvectors", 
+                sptitles = steps , colormap =cm.gray , filename = None)        
+                 
